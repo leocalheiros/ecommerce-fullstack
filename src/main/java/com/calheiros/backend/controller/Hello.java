561ee477f98322609aa98a672e6d14b1d4c073e4
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-12-26 10:02:42 -0300

    feat: add configurations files with Hello test route

---
+++ src/main/java/com/calheiros/backend/controller/Hello.java
@@ -0,0 +1,16 @@
+package com.calheiros.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World!" + new Date();
+    }
+}
